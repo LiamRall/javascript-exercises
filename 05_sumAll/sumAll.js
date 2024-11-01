743244fde686a3b2a4d67c27990dd8575db4cbc9
liam <liam@analytics.co.za>
2024-11-01 15:41:17 +0200

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return 'ERROR';
+    }
+    if(num1 % 1 !== 0 || num2 % 1 !== 0){
+        return 'ERROR';
+    }
+    var total = 0;
+    var start = Math.min(num1, num2);
+    var end = Math.max(num1, num2);
+    for(start; start <= end; start++){
+        total += start;
+    }
+    return total;
 };
 
 // Do not edit below this line
