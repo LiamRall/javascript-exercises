842e8a2cb138a7329bc1f520b1f4428aea6df204
liam <liam@analytics.co.za>
2024-11-01 15:33:01 +0200

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    var result = array;
+    for (let i = 0; i < args.length; i++) {
+        result = result.filter(item => item !== args[i]);
+    }
+    return result;
 };
 
 // Do not edit below this line
