ce58c40ccc5db8af66fc827c203b45b8b55b7dfe
liam <liam@analytics.co.za>
2024-11-12 16:35:54 +0200

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const n = parseInt(num);
+    if (n < 0) return "OOPS";
+    if (n <= 1) return n;
+    let a = 0;
+    let b = 1;
+    let c = 1;
+    for (let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
