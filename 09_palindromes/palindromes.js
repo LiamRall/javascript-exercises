216225856340121553b44051549cee45772306eb
liam <liam@analytics.co.za>
2024-11-12 16:31:49 +0200

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    cleanedWord = word.toLowerCase()
+    wordArray = cleanedWord.split('').filter(char => char.match(/[^\W_]+/));
+    reversedArray = wordArray.slice().reverse()
+    if(wordArray.join('') === reversedArray.join('')) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
