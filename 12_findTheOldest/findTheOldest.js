3da68314e26dfcfec0db837577249a634b0af2aa
liam <liam@analytics.co.za>
2024-11-12 16:44:58 +0200

    completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((oldest, currentPerson) => {
+        const oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth;
+        const currentAge = currentPerson.yearOfDeath ? currentPerson.yearOfDeath - currentPerson.yearOfBirth : new Date().getFullYear() - currentPerson.yearOfBirth;
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
 };
 
 // Do not edit below this line
